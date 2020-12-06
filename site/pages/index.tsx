c674174566a6895e04a7db7a06a19db5e95d8d1c
Nick Reynolds <me@nickreynolds.net>
2020-12-06 10:54:04 -0800

    Prettier sweep

--- site/pages/index.tsx
+++ site/pages/index.tsx
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,15 +10,18 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Hello!
-        </h1>
-        <iframe width="875" height="543" seamless frameBorder="0" scrolling="no" src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQjWkt0Rv0cVHLPg1v42h34_BXBPO9e60cziNzlVXgUJ2dxTb0GRAim38q49AkK-e2TAlc12PavJpni/pubchart?oid=1470868663&amp;format=interactive"></iframe>
+        <h1 className={styles.title}>Hello!</h1>
+        <iframe
+          width="875"
+          height="543"
+          seamless
+          frameBorder="0"
+          scrolling="no"
+          src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQjWkt0Rv0cVHLPg1v42h34_BXBPO9e60cziNzlVXgUJ2dxTb0GRAim38q49AkK-e2TAlc12PavJpni/pubchart?oid=1470868663&amp;format=interactive"
+        ></iframe>
       </main>
 
-      <footer className={styles.footer}>
-        Footer
-      </footer>
+      <footer className={styles.footer}>Footer</footer>
     </div>
-  )
+  );
 }
